d518b6dea3b9cc10acf200b2db07939582efe182
antonprikhodko93 <oxid93@bk.ru>
2022-04-19 23:11:59 +0500

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Anton Prikhodko");
         System.out.println("07.04.1993");
     }
