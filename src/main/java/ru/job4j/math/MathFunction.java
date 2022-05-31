b160cbb81524c11da289b4934688ace0e6f15c35
antonprikhodko93 <oxid93@bk.ru>
2022-06-01 00:01:27 +0500

    1.1. Что такое import [#504773] v1.1

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -4,12 +4,15 @@ public class MathFunction {
     public static double sum(double first, double second) {
         return first + second;
     }
+
     public static double multiply(double first, double second) {
         return first * second;
     }
+
     public static double subtr(double first, double second) {
         return first - second;
     }
+
     public static double div(double first, double second) {
         return first / second;
     }
