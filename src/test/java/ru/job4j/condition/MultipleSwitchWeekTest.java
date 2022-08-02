2afb7bfb1bb6a5e1a1abe78b9f20b71840b0b998
antonprikhodko93 <oxid93@bk.ru>
2022-08-02 21:56:12 +0500

    4.6.2. Switch - matcher [#334071]

---
+++ src/test/java/ru/job4j/condition/MultipleSwitchWeekTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultipleSwitchWeekTest {
+
+    @Test
+    public void nameOfПонедельник() {
+        String in = "Понедельник";
+        int result = MultipleSwitchWeek.numberOfDay(in);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void nameOfMonday() {
+        String in = "Monday";
+        int result = MultipleSwitchWeek.numberOfDay(in);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void nameOfЧетверг() {
+        String in = "Четверг";
+        int result = MultipleSwitchWeek.numberOfDay(in);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void nameOfSunday() {
+        String in = "Sunday";
+        int result = MultipleSwitchWeek.numberOfDay(in);
+        int expected = 7;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void nameOfNothingday() {
+        String in = "day";
+        int result = MultipleSwitchWeek.numberOfDay(in);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+}
